811c0ac05b9e8821130c667db7c51f93e7cba64a
Philip W <philip.whitt@sbcglobal.net>
2014-09-11 14:02:26 -0500

    DELTA-1916 mysterious dotcms structure

--- assetCheck.go
+++ assetCheck.go
@@ -12,10 +12,16 @@ type AssetsCheck struct {
 func (f *AssetsCheck) Check() (error) {
 	log.Println("Assets Checking")
 
-	rows, err := f.MySql.db.Query("SELECT c.inode " +
-									"FROM contentlet c " +
-									"JOIN field f ON c.structure_inode=f.structure_inode " +
-									"WHERE f.field_type IN ('binary', 'image', 'file');")
+	// rows, err := f.MySql.db.Query("SELECT c.inode " +
+	// 								"FROM contentlet c " +
+	// 								"JOIN field f ON c.structure_inode=f.structure_inode " +
+	// 								"WHERE f.field_type IN ('binary', 'image', 'file');")
+
+	fields, err := f.MySql.db.Query("SELECT f.structure_inode, f.field_type, f.velocity_var_name FROM field f " +
+									 "JOIN structure s ON s.inode = f.structure_inode " +
+									 "WHERE f.field_type IN ('binary', 'image', 'file') AND s.structuretype=4 ORDER BY f.structure_inode;")
+
+	defer fields.Close()
 
 	if err != nil {
 		log.Println(err)
@@ -23,28 +29,68 @@ func (f *AssetsCheck) Check() (error) {
 	}
 
 	var inode string
-	var path string
+	var assetToCheck string
+	var structure_inode string
+	var field_type string
+	var field_contentlet string
 
-	for rows.Next() {
-		rows.Scan(&inode)
+	for fields.Next() {
+		fields.Scan(&structure_inode, &field_type, &field_contentlet)
 
-		// @todo make this a config param
-		path = "/var/bv/apps/dotcms/assets/" + inode[0:1] + "/" + inode[1:2] + "/" + inode
+		// if field_type == "binary" {
+		// 	continue
+		// }
 
-		// log.Println(inode + " -> " + path)
+		// log.Println(structure_inode + ", " + field_type + ", " + field_contentlet)		
+		// log.Println("Checking " + structure_inode + "....")
+		// log.Println("SELECT inode, "+field_contentlet+" AS assetToCheck FROM contentlet WHERE structure_inode='"+structure_inode+"'")
 
-		exixsts, _ := exists(path)
+		// contentlets, err := f.MySql.db.Query("SELECT inode, "+field_contentlet+" AS assetToCheck FROM contentlet WHERE structure_inode=?", structure_inode)
+		contentlets, err := f.MySql.db.Query("SELECT inode, text3 AS assetToCheck FROM contentlet WHERE structure_inode=?", structure_inode)
+
+		defer contentlets.Close()
 
 		if err != nil {
 			log.Println(err)
 			os.Exit(1)
 		}
 
-		if exixsts == true {
-			log.Println("Inode exists")
-		} else {
-			log.Println("NOT FOUND: " + inode)
+		for contentlets.Next() {
+			contentlets.Scan(&inode, &assetToCheck)
+
+			if assetToCheck != "" {
+				path := "/var/bv/apps/dotcms/assets/" + inode[0:1] + "/" + inode[1:2] + "/" + inode + "/" + field_contentlet + "/" + assetToCheck
+
+				exixsts, _ := exists(path)
+
+				if exixsts == true {
+					log.Println("Inode exists")
+				} else {
+					log.Println("NOT FOUND! Contentlet: " + inode)
+				}
+
+				// log.Println("	" + path)
+			}
 		}
+
+		// for contentlets
+		// @todo make this a config param
+		// path = "/var/bv/apps/dotcms/assets/" + inode[0:1] + "/" + inode[1:2] + "/" + inode
+
+		// // log.Println(inode + " -> " + path)
+
+		// exixsts, _ := exists(path)
+
+		// if err != nil {
+		// 	log.Println(err)
+		// 	os.Exit(1)
+		// }
+
+		// if exixsts == true {
+		// 	log.Println("Inode exists")
+		// } else {
+		// 	log.Println("NOT FOUND: " + inode)
+		// }
 	}
 
 	log.Println("Done")

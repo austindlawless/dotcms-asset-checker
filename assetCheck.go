35010bc79829b91a9f359f170991eda8b6d31d4c
Philip W <philip.whitt@sbcglobal.net>
2014-09-11 15:11:44 -0500

    DELTA-1916 selecting only working nodes

--- assetCheck.go
+++ assetCheck.go
@@ -12,11 +12,6 @@ type AssetsCheck struct {
 func (f *AssetsCheck) Check() (error) {
 	log.Println("Assets Checking")
 
-	// rows, err := f.MySql.db.Query("SELECT c.inode " +
-	// 								"FROM contentlet c " +
-	// 								"JOIN field f ON c.structure_inode=f.structure_inode " +
-	// 								"WHERE f.field_type IN ('binary', 'image', 'file');")
-
 	fields, err := f.MySql.db.Query("SELECT f.structure_inode, f.field_type, f.velocity_var_name FROM field f " +
 									 "JOIN structure s ON s.inode = f.structure_inode " +
 									 "WHERE f.field_type IN ('binary', 'image', 'file') AND s.structuretype=4 ORDER BY f.structure_inode;")
@@ -31,22 +26,18 @@ func (f *AssetsCheck) Check() (error) {
 	var inode string
 	var assetToCheck string
 	var structure_inode string
+	var identifier string
 	var field_type string
 	var field_contentlet string
 
 	for fields.Next() {
 		fields.Scan(&structure_inode, &field_type, &field_contentlet)
 
-		// if field_type == "binary" {
-		// 	continue
-		// }
-
-		// log.Println(structure_inode + ", " + field_type + ", " + field_contentlet)		
-		// log.Println("Checking " + structure_inode + "....")
-		// log.Println("SELECT inode, "+field_contentlet+" AS assetToCheck FROM contentlet WHERE structure_inode='"+structure_inode+"'")
-
-		// contentlets, err := f.MySql.db.Query("SELECT inode, "+field_contentlet+" AS assetToCheck FROM contentlet WHERE structure_inode=?", structure_inode)
-		contentlets, err := f.MySql.db.Query("SELECT inode, text3 AS assetToCheck FROM contentlet WHERE structure_inode=?", structure_inode)
+		// Only select working nodes
+		contentlets, err := f.MySql.db.Query("SELECT cl.inode, cl.identifier, cl.text3 AS assetToCheck " +
+											 "FROM contentlet cl " +
+											 "JOIN contentlet_version_info c ON c.identifier=cl.identifier AND c.working_inode=cl.inode "+
+											 "WHERE structure_inode=?", structure_inode)
 
 		defer contentlets.Close()
 
@@ -56,9 +47,10 @@ func (f *AssetsCheck) Check() (error) {
 		}
 
 		for contentlets.Next() {
-			contentlets.Scan(&inode, &assetToCheck)
+			contentlets.Scan(&inode, &identifier, &assetToCheck)
 
 			if assetToCheck != "" {
+				// @todo make this a config param
 				path := "/var/bv/apps/dotcms/assets/" + inode[0:1] + "/" + inode[1:2] + "/" + inode + "/" + field_contentlet + "/" + assetToCheck
 
 				exixsts, _ := exists(path)
@@ -74,25 +66,6 @@ func (f *AssetsCheck) Check() (error) {
 				// log.Println("	" + path)
 			}
 		}
-
-		// for contentlets
-		// @todo make this a config param
-		// path = "/var/bv/apps/dotcms/assets/" + inode[0:1] + "/" + inode[1:2] + "/" + inode
-
-		// // log.Println(inode + " -> " + path)
-
-		// exixsts, _ := exists(path)
-
-		// if err != nil {
-		// 	log.Println(err)
-		// 	os.Exit(1)
-		// }
-
-		// if exixsts == true {
-		// 	log.Println("Inode exists")
-		// } else {
-		// 	log.Println("NOT FOUND: " + inode)
-		// }
 	}
 
 	log.Println("Done")
